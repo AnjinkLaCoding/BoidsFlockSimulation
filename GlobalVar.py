d42cc2aebbc5b4663bd90baa6d9fa235ae6baeec
Matthew <152848985+AnjinkLaCoding@users.noreply.github.com>
2025-10-21 14:13:38 +0800

    Update GlobalVar.py

--- GlobalVar.py
+++ GlobalVar.py
@@ -2,10 +2,10 @@ def init():
     global boids_array, obstacle_array, WIDTH, HEIGHT, FPS
     #global speed_adjustment
 
-    boids_array = []
-    obstacle_array = []
-    #speed_adjustment = 0
+    boids_array = [] #To store the boids object
+    obstacle_array = [] #To store the obstacle objects
+    #speed_adjustment = 0 #To adjust the speed of the boids (if needed just uncomment)
 
-    WIDTH = 1500
-    HEIGHT = 800
-    FPS = 60
+    WIDTH = 1500 #Width of the simulation display
+    HEIGHT = 800 #Height of the simulation display
+    FPS = 60 #FPS of the simulation display
