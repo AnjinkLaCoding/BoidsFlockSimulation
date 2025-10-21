276fd145f6f8c145033ff853d5d7600a7110e868
Matthew <152848985+AnjinkLaCoding@users.noreply.github.com>
2025-10-21 14:00:01 +0800

    Rename Agent class to Boids and update methods

--- Boids.py
+++ Boids.py
@@ -2,14 +2,14 @@ from math import sqrt
 
 DEFAULT_SPEED = 4
 
-class Agent(object):
+class Boids(object):
 
     def __init__(self,pos = (1,1), vel=(DEFAULT_SPEED,0)):
         self.pos = (pos[0],pos[1])
         self.vel = vel
 
-    def distance_from(self,other_agent):
-        return sqrt((other_agent.pos[0] - self.pos[0])**2 + (other_agent.pos[1] - self.pos[1]) **2)
+    def boids_dist(self,other_boids):
+        return sqrt((other_boids.pos[0] - self.pos[0])**2 + (other_boids.pos[1] - self.pos[1]) **2)
 
     def update_pos(self):
         self.pos = self.pos[0] + self.vel[0], self.pos[1] + self.vel[1]
