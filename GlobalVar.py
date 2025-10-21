c1ae73fb4731216b5ecfd158eecafb761b6b97d7
Matthew <152848985+AnjinkLaCoding@users.noreply.github.com>
2025-10-21 14:02:19 +0800

    Create GlobalVar.py

---
+++ GlobalVar.py
@@ -0,0 +1,10 @@
+def init():
+    global boids_array, obstacle_array, speed_adjustment, WIDTH, HEIGHT, FPS
+
+    agent_array = []
+    obstacle_array = []
+    speed_adjustment = 0
+
+    WIDTH = 1500
+    HEIGHT = 800
+    FPS = 60
