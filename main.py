0b8d951774e7c4828f67220be25c4c3a9127b36d
PakyinCoding <157379643+PakyinCoding@users.noreply.github.com>
2025-10-21 16:24:18 +0800

    Initialize boids and predators in main.py

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ pygame.init()
 display_screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
+boids = [Boid((random.uniform(0, WIDTH), random.uniform(0, HEIGHT))) for _ in range(BOID_COUNT)
+predators = [Predator((random.uniform(0, WIDTH), random.uniform(0,HEIGHT))) for _ in range(PREDATOR_COUNT)
+
