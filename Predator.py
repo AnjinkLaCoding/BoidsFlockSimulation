8f41f2bd43853ba866e0255e20f8545c47eacbee
JasonSupala <supala.jason@gmail.com>
2025-10-21 16:25:20 +0800

    Added Predator.py

---
+++ Predator.py
@@ -0,0 +1,33 @@
+from random import randint
+from Boids_Agent import Boids
+import math
+import pygame
+
+def limit_vector(v, max_value):
+    mag = math.sqrt(v[0]**2 + v[1]**2)
+    if mag > max_value and mag != 0:
+        return (v[0] / mag * max_value, v[1] / mag * max_value)
+    return v
+
+class Predator(Boids):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.mass = randint(8, 15)
+        self.max_force = 10
+
+    def approach(self, boids):
+        count = 0
+        loc_sum = (0, 0)
+        for other in boids:
+            d = math.sqrt((self.pos[0] - other.pos[0])**2 + (self.pos[1] - other.pos[1])**2)
+            if 0 < d < self.mass + 260:
+                loc_sum = (loc_sum[0] + other.pos[0], loc_sum[1] + other.pos[1])
+                count += 1
+        if count > 0:
+            loc_sum = (loc_sum[0] / count, loc_sum[1] / count)
+            approach_vec = (loc_sum[0] - self.pos[0], loc_sum[1] - self.pos[1])
+            approach_vec = limit_vector(approach_vec, self.max_force)
+            self.apply_force(approach_vec)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, PRED_COLOR, (int(self.pos[0]), int(self.pos[1])), self.mass)
