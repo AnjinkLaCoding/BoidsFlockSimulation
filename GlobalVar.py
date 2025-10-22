0627181f6e17c8429045bc4ce68e1398162685da
Matthew <152848985+AnjinkLaCoding@users.noreply.github.com>
2025-10-22 22:44:28 +0800

    Update GlobalVar.py

--- GlobalVar.py
+++ GlobalVar.py
@@ -3,15 +3,23 @@ def init():
     global show_arrows, show_flocking, show_circles, show_predators, show_obstacle, obstacle_pos
     global BG, BOID_COLOR, PRED_COLOR
 
+    #Storing agents
     agent_array = []
+    #To store obstacles
     obstacle_array = []
+    #To adjust the speed of the biods (if needed)
     speed_adjustment = 0
 
+    #Width and height of the display
     WIDTH, HEIGHT = 1000, 800
+    #Default boids
     BOID_COUNT = 10
+    #Default predators
     PREDATOR_COUNT = 2
+    #Obstacles radius
     OBSTACLE_RADIUS = 150
 
+    #Flag for the features
     show_flocking = True
     show_arrows = True
     show_circles = False
@@ -19,6 +27,7 @@ def init():
     show_obstacle = False
     obstacle_pos = None
 
+    #Background Configuration
     BG = (200, 220, 255)
     BOID_COLOR = (30, 30, 30)
     PRED_COLOR = (255, 120, 100)
