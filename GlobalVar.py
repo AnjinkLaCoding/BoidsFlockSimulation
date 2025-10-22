282231d4f81135b8f73e0012920b94e496b26dd0
Matthew <152848985+AnjinkLaCoding@users.noreply.github.com>
2025-10-22 22:38:31 +0800

    Modify obstacle parameters in GlobalVar.py
    Added 'obstacle_pos' variable and increased 'OBSTACLE_RADIUS'.

--- GlobalVar.py
+++ GlobalVar.py
@@ -1,6 +1,6 @@
 def init():
     global agent_array, obstacle_array, speed_adjustment, WIDTH, HEIGHT, BOID_COUNT, PREDATOR_COUNT, OBSTACLE_RADIUS
-    global show_arrows, show_flocking, show_circles, show_predators, show_obstacle
+    global show_arrows, show_flocking, show_circles, show_predators, show_obstacle, obstacle_pos
     global BG, BOID_COLOR, PRED_COLOR
 
     agent_array = []
@@ -10,13 +10,14 @@ def init():
     WIDTH, HEIGHT = 1000, 800
     BOID_COUNT = 10
     PREDATOR_COUNT = 2
-    OBSTACLE_RADIUS = 60
+    OBSTACLE_RADIUS = 150
 
     show_flocking = True
     show_arrows = True
     show_circles = False
     show_predators = True
     show_obstacle = False
+    obstacle_pos = None
 
     BG = (200, 220, 255)
     BOID_COLOR = (30, 30, 30)
